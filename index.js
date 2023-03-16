749f76883a441138e936c14a80f47f1d64cf0034
lytoooo0 <lytoooo0@icloud.com>
2023-03-16 17:40:50 +0100

    add new york time

--- index.js
+++ index.js
@@ -18,27 +18,36 @@ function formatTimeOf(time){
 
 const request_link = "https://worldtimeapi.org/api/timezone/";
 const eu = "Europe/Stockholm";
-const us = "America/Los_Angeles";
+const la = "America/Los_Angeles";
+const ny = "America/New_York";
 const cn = "Asia/Shanghai";
 
 var eu_time_data = JSON.parse(httpGet(request_link+eu));
-var us_time_data = JSON.parse(httpGet(request_link+us));
+var la_time_data = JSON.parse(httpGet(request_link+la));
+var ny_time_data = JSON.parse(httpGet(request_link+ny));
 var cn_time_data = JSON.parse(httpGet(request_link+cn));
 
-var us_time = new Date(us_time_data.datetime.split('.')[0]);
+var localtime = Date();
+
+var ny_time = new Date(ny_time_data.datetime.split('.')[0]);
+var la_time = new Date(la_time_data.datetime.split('.')[0]);
 var eu_time = new Date(eu_time_data.datetime.split('.')[0]);
 var cn_time = new Date(cn_time_data.datetime.split('.')[0]);
 
 function updateTime() {
-    document.getElementById('us_time').innerHTML = "美国: " + formatTimeOf(us_time);
-    document.getElementById('eu_time').innerHTML = "瑞典: " + formatTimeOf(eu_time);
-    document.getElementById('cn_time').innerHTML = "中国: " + formatTimeOf(cn_time);
-    var eu_us = (eu_time_data.raw_offset - us_time_data.raw_offset)/3600
-    var cn_eu = (cn_time_data.raw_offset - eu_time_data.raw_offset)/3600
-    document.getElementById('eu_us_offset').innerHTML = "+" + eu_us;
+    document.getElementById('la_time').innerHTML = "加州: " + formatTimeOf(la_time);
+    document.getElementById('ny_time').innerHTML = "纽约: " + formatTimeOf(ny_time);
+    document.getElementById('eu_time').innerHTML = "斯京: " + formatTimeOf(eu_time);
+    document.getElementById('cn_time').innerHTML = "深圳: " + formatTimeOf(cn_time);
+    var ny_la = (ny_time_data.raw_offset - la_time_data.raw_offset)/3600;
+    var eu_ny = (eu_time_data.raw_offset - ny_time_data.raw_offset)/3600;
+    var cn_eu = (cn_time_data.raw_offset - eu_time_data.raw_offset)/3600;
+    document.getElementById('ny_la_offset').innerHTML = "+" + ny_la;
+    document.getElementById('eu_ny_offset').innerHTML = "+" + eu_ny;
     document.getElementById('eu_cn_offset').innerHTML = "+" + cn_eu;
 
-    us_time.setTime(us_time.getTime() + 1000);
+    la_time.setTime(la_time.getTime() + 1000);
+    ny_time.setTime(ny_time.getTime() + 1000);
     eu_time.setTime(eu_time.getTime() + 1000);
     cn_time.setTime(cn_time.getTime() + 1000);
 };
